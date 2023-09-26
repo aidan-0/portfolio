bdde7612801c0e15355e7d1e5a4d0744d84eef4a
Aidan <am.mcdonald00@gmail.com>
2023-09-26 20:36:11 +0800

    scrolling nav idea wasnt working. new design now

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import AboutMe from './nav_components/about_me_components/AboutMe';
 import Resume from './nav_components/resume_components/Resume'; 
+import Projects from './nav_components/projects_components/Projects'
+import Contact from './nav_components/contact_components/Contact'
 
 export default function Content() {
   return (
     <div className="content-container">
-      <h2 className="content-header">(display content heading)</h2>
+      {/* <h2 className="content-header">(display content heading)</h2> */}
       <div className="content-card">
         <Routes>
           <Route path="aboutme" element={<AboutMe />} />
           <Route path="resume" element={<Resume />} />
-          {/* You can add more routes as needed */}
+          <Route path="projects" element={<Projects />} />
+          <Route path="contact" element={<Contact />} />
         </Routes>
       </div>
     </div>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,32 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  
   return (
     <nav className="scrolling-nav">
-      <ul>
-        <li><Link to="aboutme">About Me</Link></li>
-        <li><Link to="resume">Resume</Link></li>
+      <ul className="nav-list" id="nav-list" data-mouse-down-at="0">
+      <li>
+          <Link to="aboutme" className="link-style">
+            About Me
+          </Link>
+        </li>
+        <li>
+          <Link to="resume" className="link-style">
+            Resume
+          </Link>
+        </li>
+        <li>
+          <Link to="projects" className="link-style">
+            Projects
+          </Link>
+        </li>
+        {/* when clicking on projects, make the nav bar show the full list of projects and have it scrollable like the nav */}
+        <li>
+          <Link to="contact" className="link-style">
+            Contact
+          </Link>
+        </li>
+        {/* clicking on contact provides a modal with all contact details */}
       </ul>
     </nav>
   );
