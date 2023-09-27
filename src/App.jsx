e64a371d18cf8f54c07be6821d8810b27590a8f1
Aidan <am.mcdonald00@gmail.com>
2023-09-27 11:19:53 +0800

    new nav and bg styling. added some accent styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-import React from 'react';
-import Header from './Header';
+import React, {useEffect, useState} from 'react';
+// import Header from './Header';
 import Content from './Content';
 import Navbar from './Navbar';
 
 export default function App() {
+    const [y, setY] = useState(0);
+  
+    const handleMouseMove = (e) => {
+      setY(e.clientY);
+    };
+  
+    useEffect(() => {
+      window.addEventListener('mousemove', handleMouseMove);
+  
+      return () => {
+        window.removeEventListener('mousemove', handleMouseMove);
+      };
+    }, []);
+
+
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      document.body.style.setProperty('--mouse-x', e.clientX + 'px');
+      document.body.style.setProperty('--mouse-y', e.clientY + 'px');
+    }
+    
+    window.addEventListener('mousemove', handleMouseMove);
+    
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  }, []);
+
+
+
+
   return (
     <div className='page-content'>
-      <Header />
+      <div className="follower--left" style={{ top: y }}></div>
+      <div className="follower--right" style={{ top: y }}></div>
+      {/* <Header /> */}
       <div className='main-content'>
         <Navbar />
         <Content />
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,33 +1,42 @@
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  
   return (
-    <nav className="scrolling-nav">
-      <ul className="nav-list" id="nav-list" data-mouse-down-at="0">
-      <li>
-          <Link to="aboutme" className="link-style">
-            About Me
-          </Link>
-        </li>
-        <li>
-          <Link to="resume" className="link-style">
-            Resume
-          </Link>
-        </li>
-        <li>
-          <Link to="projects" className="link-style">
-            Projects
-          </Link>
-        </li>
-        {/* when clicking on projects, make the nav bar show the full list of projects and have it scrollable like the nav */}
-        <li>
-          <Link to="contact" className="link-style">
-            Contact
-          </Link>
-        </li>
-        {/* clicking on contact provides a modal with all contact details */}
-      </ul>
-    </nav>
+    <div className="wrapper">
+      <nav className="nav">
+        <ul className="nav-list" id="nav-list" data-mouse-down-at="0">
+          <li className="li--aboutme">
+            <span className="link-wrapper">
+              <Link to="aboutme" className="link-style">
+                About Me
+              </Link>
+            </span>
+          </li>
+          <li className="li--resume">
+            <span className="link-wrapper">
+              <Link to="resume" className="link-style">
+                Resume
+              </Link>
+            </span>
+          </li>
+          <li className="li--projects">
+            <span className="link-wrapper">
+              <Link to="projects" className="link-style">
+                Projects
+              </Link>
+            </span>
+          </li>
+          {/* when clicking on projects, make the nav bar show the full list of projects and have it scrollable like the nav */}
+          <li className="li--contact">
+            <span className="link-wrapper">
+              <Link to="contact" className="link-style">
+                Contact
+              </Link>
+            </span>
+          </li>
+          {/* clicking on contact provides a modal with all contact details */}
+        </ul>
+      </nav>
+    </div>
   );
 }
