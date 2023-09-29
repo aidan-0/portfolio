29e89e91de3bc417f4cf8f206ac3a672a38eb052
Aidan <am.mcdonald00@gmail.com>
2023-09-29 22:53:39 +0800

    added work experience

--- src/nav_components/resume_components/Resume.jsx
+++ src/nav_components/resume_components/Resume.jsx
@@ -15,21 +15,32 @@ export default function Resume() {
         <p className="position">Volunteer Developer</p>
         <ul className="duties">
           <li>
-            Working with Community Spirit Foundation to develop 'Stride For
-            Education' a progressive web app built with Vue, Vuetify, and
-            Django.
+            Worked with Community Spirit Foundation to develop
+            <span className="website-link">
+              <a
+                href="https://stride-for-education.vercel.app/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Stride For Education
+              </a>
+            </span>
+            a progressive web app built with Vue, Vuetify, and Django for their
+            Winter 2023 Project.
           </li>
           <li>
-            Led the development of the team dashboard page and login modal to
-            enhance the user interface and experience.
+            Led the design and development of the team dashboard and login
+            modal, focusing on user-centric design principles to ensure ease of
+            navigation and usage.
           </li>
           <li>
-            Developed and implemented comprehensive back-end tests to ensure
-            software quality and functionality.
+            Developed back-end tests, addressing various scenarios and edge
+            cases to assess the application's stability, and functionality.
           </li>
           <li>
-            Optimised web performance, resulting in an improved website
-            performance score.
+            Refined the codebase, optimising resource loading, and implementing
+            best practices in web performance, to ensure the application ran
+            smoothly and efficiently.
           </li>
         </ul>
       </div>
@@ -37,38 +48,108 @@ export default function Resume() {
       {/* AR / A3D */}
       <div className="job">
         <div className="company-and-date">
-          <h3>Coders For Causes</h3>
-          <h3>June 2023-July 2023</h3>
+          <h3>Asset Reports</h3>
+          <h3>Feb 2021-Present</h3>
         </div>
 
-        <p className="position">Volunteer Developer</p>
+        {/* Asset 3D */}
+        <p className="position">
+          Head of Sales & Customer Support for Asset 3D - Jan 2023-Present
+        </p>
         <ul className="duties">
           <li>
-            Working with Community Spirit Foundation to develop 'Stride For
-            Education' a progressive web app built with Vue, Vuetify, and
-            Django.
+            Led the successful initiative to become an Official Reseller of
+            Matterport equipment and SaaS accounts, overseeing all aspects of
+            project execution including planning, coordinating equipment
+            purchases and sales.
           </li>
           <li>
-            Led the development of the team dashboard page and login modal to
-            enhance the user interface and experience.
+            Drafted comprehensive business plans, establishing clear Marketing &
+            Sales targets to guide organisational efforts. Coordinated and
+            conducted extensive team training sessions across Perth, Melbourne,
+            and Sydney, ensuring uniformity in skills and knowledge.
           </li>
           <li>
-            Developed and implemented comprehensive back-end tests to ensure
-            software quality and functionality.
+            Directed the design and execution of impactful marketing materials
+            and EDM campaigns, establishing brand presence and reaching targeted
+            audiences effectively.
           </li>
           <li>
-            Optimised web performance, resulting in an improved website
-            performance score.
+            Utilised Squarespace, CSS, and JS to develop and maintain
+            high-quality websites, implementing SEO strategies to enhance online
+            presence and user experience.
+          </li>
+        </ul>
+
+        {/* Asset Reports */}
+        <p className="position">
+          Head of Virtual Tours Department - Nov 2021-Present
+        </p>
+        <ul className="duties">
+          <li>
+            Managed and supported a team of 7 across three states, coordinating
+            bookings and resolving operational issues. Conducted regular
+            check-ins to foster a positive work environment.
+          </li>
+          <li>
+            Maintained proactive engagement with stakeholders by consistently
+            delivering high-quality deliverables, and collaborating with clients
+            to understand their marketing needs, developing tailored content and
+            branding materials using Adobe Illustrator and Photoshop
+          </li>
+          <li>
+            Provided Level 1 & 2 support to resolve user issues in proprietary
+            software systems
+          </li>
+          <li>
+            Initiated and monitored quality control measures within the
+            department to assure high standards, implementing corrective actions
+            as necessary.
+          </li>
+          <li>
+            Maintained and updated all company websites, including{" "}
+            <span className="website-link">
+              <a
+                href="https://www.reportsonline.com.au/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Reports Online
+              </a>
+            </span>
+            ,{" "}
+            <span className="website-link">
+              <a
+                href="https://houselandmaps.com.au/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                House & Land Maps
+              </a>
+            </span>{" "}
+            and{" "}
+            <span className="website-link">
+              <a
+                href="https://www.asset3d.com.au/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Asset 3D
+              </a>
+            </span>
+            , while optimising SEO to ensure a strong online presence.
+          </li>
+          <li>
+            Salesforce Key User - designed EDMs, generated detailed reports,
+            created dashboards for management, and input user data.
+          </li>
+          <li>
+            Directed both personal and professional development initiatives
+            within the Virtual Tours department, coordinating comprehensive
+            onboarding and ongoing training for staff.
           </li>
         </ul>
       </div>
-
-
-      <h3>Asset Reports</h3>
-      <p>Jan 2021 - Present</p>
-      <p>Head of Sales & Customer Support</p>
-      <p>Head of Virtual Tours Department</p>
-      <p>On-site Virtual Tours Operator</p>
 
       {/* push to bottom of page */}
       <button>Download PDF of my Resume</button>
