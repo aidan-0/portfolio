68178e86f481c469a557f15e7489fe6fe0988694
Aidan <am.mcdonald00@gmail.com>
2023-10-03 13:48:03 +0800

    load animations for home page

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,30 +4,30 @@ export default function Navbar() {
   return (
     <div className="wrapper">
       <nav className="nav">
-        <ul className="nav-list" id="nav-list" data-mouse-down-at="0">
+        <ul className="nav-list">
           <li className="li--aboutme">
-            <span className="link-wrapper">
+            <span className="link-wrapper wrapper--aboutme">
               <Link to="aboutme" className="link-style">
                 About Me
               </Link>
             </span>
           </li>
           <li className="li--resume">
-            <span className="link-wrapper">
+            <span className="link-wrapper wrapper--resume">
               <Link to="resume" className="link-style">
                 Resume
               </Link>
             </span>
           </li>
           <li className="li--projects">
-            <span className="link-wrapper">
+            <span className="link-wrapper wrapper--projects">
               <Link to="projects" className="link-style">
                 Projects
               </Link>
             </span>
           </li>
           <li className="li--contact">
-            <span className="link-wrapper">
+            <span className="link-wrapper wrapper--contact">
               <Link to="contact" className="link-style">
                 Contact
               </Link>
