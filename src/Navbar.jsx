77d808968cdd2c0e70e066e11a150daf1d1f3f6a
Aidan <am.mcdonald00@gmail.com>
2023-10-05 22:00:18 +0800

    hamburger menu

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,38 +4,47 @@ import Contact from "./nav_components/contact_components/Contact";
 
 export default function Navbar() {
   const [showModal, setShowModal] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   function handleContactClick() {
     console.log("contact clicked");
     setShowModal(!showModal);
   }
 
+  function handleMenuClick() {
+    console.log("menu clicked");
+    setMenuOpen(!menuOpen);
+  }
+
   return (
     <div className="wrapper">
       <nav className="nav">
-        <ul className="nav-list">
-          <li className="li--aboutme">
+        <div className="hamburger" onClick={handleMenuClick}>
+          ☰
+        </div>
+        <ul className={`nav-list ${menuOpen ? "open" : ""}`}>
+          <li className="li--aboutme" onClick={handleMenuClick}>
             <span className="link-wrapper wrapper--aboutme">
               <Link to="aboutme" className="link-style">
                 About Me
               </Link>
             </span>
           </li>
-          <li className="li--resume">
+          <li className="li--resume" onClick={handleMenuClick}>
             <span className="link-wrapper wrapper--resume">
               <Link to="resume" className="link-style">
                 Resume
               </Link>
             </span>
           </li>
-          <li className="li--projects">
+          <li className="li--projects" onClick={handleMenuClick}>
             <span className="link-wrapper wrapper--projects">
               <Link to="projects" className="link-style">
                 Projects
               </Link>
             </span>
           </li>
-          <li className="li--contact">
+          <li className="li--contact" onClick={handleMenuClick}>
             <span className="link-wrapper wrapper--contact">
               <button
                 className="link-style contact-modal-btn"
@@ -47,7 +56,7 @@ export default function Navbar() {
           </li>
         </ul>
       </nav>
-      {showModal && (<Contact onClose={() => setShowModal(false)} />)}
+      {showModal && <Contact onClose={() => setShowModal(false)} />}
     </div>
   );
 }
